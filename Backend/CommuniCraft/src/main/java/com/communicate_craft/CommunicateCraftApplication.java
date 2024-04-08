16ee17d67659fb6a81438e8f4b7c0b0d94dc9d27
amjadkayed <amjad.kayed@hotmail.com>
2024-04-08 04:04:09 +0300

    init: spring boot project with yml file

---
+++ Backend/CommuniCraft/src/main/java/com/communicate_craft/CommunicateCraftApplication.java
@@ -0,0 +1,13 @@
+package com.communicate_craft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunicateCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunicateCraftApplication.class, args);
+    }
+
+}
---
+++ Backend/CommuniCraft/src/test/java/com/communicate_craft/CommuniCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communicate_craft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuniCraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
