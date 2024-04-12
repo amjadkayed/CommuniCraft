c2643bd4260ac2af93104bbba4a337618bd4397e
amjadkayed <amjad.kayed@hotmail.com>
2024-04-12 17:24:55 +0300

    update: finish all Security configurations

--- Backend/CommuniCraft/src/main/java/com/communicate_craft/config/SecurityConfig.java
+++ Backend/CommuniCraft/src/main/java/com/communicate_craft/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,5 +39,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-
 }
